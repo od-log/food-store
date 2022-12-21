7a360e9fb5a3605ebaab1d41f61fd4b9fb074082
OD <kg-te@naver.com>
2022-12-22 01:26:27 +0900

    :tada: 패키지 및 기본 설정 초기화

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/images.brandsites/common-ground/uploads/**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
